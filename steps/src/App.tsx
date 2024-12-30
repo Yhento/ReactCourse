1bdec992ba812fb5bb64e2125095988aa98741ad
Tyler Payne <154254574+Yhento@users.noreply.github.com>
2024-12-30 10:42:34 -0600

    finished step practice.

--- steps/src/App.tsx
+++ steps/src/App.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const messages = [
   "Learn React ⚛️",
   "Apply for jobs 💼",
@@ -5,27 +7,61 @@ const messages = [
 ];
 
 function App() {
-  const step = 1;
-
   return (
+    <>
+      <Steps />
+      <Steps />
+    </>
+  );
+}
+
+function Steps() {
+  const [step, setStep] = useState(1);
+  // const [test, setTest] = useState({ name: "Tyler" });
+  const [isOpen, setIsOpen] = useState(true);
+
+  function handlePrevious() {
+    if (step > 1) setStep((s) => s - 1);
+  }
+
+  function handleNext() {
+    if (step < 3) setStep((s) => s + 1);
+    // setTest({ name: "John" });
+  }
+
+  return isOpen ? (
     <div className="steps">
       <div className="numbers">
-        <div className={`${step >= 1 ? "active" : "null"}`}>1</div>
-        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
-        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+        <button className="close" onClick={() => setIsOpen((is) => !is)}>
+          X
+        </button>
+        <div className={step >= 1 ? "active" : ""}>1</div>
+        <div className={step >= 2 ? "active" : ""}>2</div>
+        <div className={step >= 3 ? "active" : ""}>3</div>
       </div>
       <p className="message">
         Step {step}: {messages[step - 1]}
+        {/* {test.name} */}
       </p>
       <div className="buttons">
-        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={handlePrevious}
+        >
           Previous
         </button>
-        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={handleNext}
+        >
           Next
         </button>
       </div>
     </div>
+  ) : (
+    <button className="close" onClick={() => setIsOpen((is) => !is)}>
+      X
+    </button>
   );
 }
 
